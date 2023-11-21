563e8a021e2f4e381bda670f7f8882a8f82cb2e6
Eulyses03 <eulyses.betasolo@bisu.edu.ph>
2023-11-21 09:33:29 +0800

    new file:   App.js 	deleted:    App.tsx 	modified:   package-lock.json 	modified:   package.json 	new file:   src/screens/Signin.js

---
+++ App.js
@@ -0,0 +1,18 @@
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>View App</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+  },
+});
+
+export default App;
