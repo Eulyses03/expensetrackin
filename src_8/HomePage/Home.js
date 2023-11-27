481e721698534894fe293f369079b7b2832fd593
Eulyses03 <eulyses.betasolo@bisu.edu.ph>
2023-11-28 01:54:05 +0800

    modified:   package-lock.json 	modified:   package.json 	modified:   src_8/HomePage/Home.js

--- src_8/HomePage/Home.js
+++ src_8/HomePage/Home.js
@@ -12,7 +12,6 @@ import CustomButton from '../../src_3/custombutton/CustomButton';
 import CustomButton_2 from '../../src_3/custombutton/custombutton_2/CustomButton_2';
 import CustomButton_3 from '../../src_3/colorbutton/CustomButton_3';
 import CustomButton_4 from '../../src_7/welcomepage/CustomButton_4/CustomButton_4';
-import Logo from '../HomePage/Logo_1.png';
 import {useNavigation} from '@react-navigation/native';
 
 const Home = () => {
@@ -25,6 +24,16 @@ const Home = () => {
 
     navigation.navigate('Sign in');
   };
+  const onResentPressed = () => {
+    console.warn('Resending Code');
+  };
+  const onBackPressed = Signinscreen => {
+    console.warn('Back to sign in page');
+  };
+  const onSigninPressed = () => {
+    console.warn('Signing');
+  };
+
   const {height} = useWindowDimensions();
   const navigation = useNavigation();
   return (
@@ -38,17 +47,21 @@ const Home = () => {
 
 const styles = StyleSheet.create({
   root: {
+    padding: 20,
+    marginVertical: '10%',
     alignItems: 'center',
   },
   title: {
     textAlign: 'left',
-    fontSize: 30,
+    fontSize: 74,
     fontWeight: 'bold',
     color: '#051c60',
     margin: 10,
   },
   Text: {
     color: 'gray',
+    padding: 5,
+    marginVertical: 10,
   },
   link: {
     color: '#fdb075',
