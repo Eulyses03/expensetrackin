2ce9cd68491e837efc64f58538e265b716e79cf4
Eulyses03 <eulyses.betasolo@bisu.edu.ph>
2023-12-07 15:31:56 +0800

    modified:   App.js 	deleted:    Navigation/MainContainer.js 	deleted:    Navigation/Navi.js 	deleted:    Navigation/screens/AccountScreen.js 	deleted:    Navigation/screens/DetailsScreen.js 	deleted:    Navigation/screens/HomeScreen.js 	deleted:    Navigation/screens/SettingsScreen.js 	modified:   package-lock.json 	modified:   package.json 	new file:   src/Screens/AccountScreen.js 	new file:   src/Screens/AddExpense.js 	new file:   src/Screens/HomeScreen.js 	new file:   src/Screens/ViewExpense.js 	deleted:    src/screens/Signinscreen.js 	deleted:    src/screens/assets/images/Logo_1.png 	deleted:    src/screens/index.js 	deleted:    src_10/ViewLiquidation/BottomPage.js 	deleted:    src_10/ViewLiquidation/index.js 	deleted:    src_2/components/customInput/CustomInput.js 	deleted:    src_2/components/customInput/index.js 	deleted:    src_3/colorbutton/CustomButton_3.js 	deleted:    src_3/colorbutton/index.js 	deleted:    src_3/custombutton/CustomButton.js 	deleted:    src_3/custombutton/custombutton_2/CustomButton_2.js 	deleted:    src_3/custombutton/custombutton_2/index.js 	deleted:    src_3/custombutton/index.js 	deleted:    src_4/Signupscreen/Signupscreen.js 	deleted:    src_4/Signupscreen/index.js 	deleted:    src_5/confrimAccount/ConfirmEmail.js 	deleted:    src_5/confrimAccount/index copy.js 	deleted:    src_6/ResetPassword/ResetPassword.js 	deleted:    src_6/ResetPassword/index copy.js 	deleted:    src_7/welcomepage/CustomButton_4/CustomButton_4.js 	deleted:    src_7/welcomepage/CustomButton_4/Logo_1.png 	deleted:    src_7/welcomepage/CustomButton_4/index_2.js 	deleted:    src_7/welcomepage/WelcomePage.js 	deleted:    src_7/welcomepage/index.js 	deleted:    src_8/HomePage/HomeMain.js 	deleted:    src_8/HomePage/Logo/Logo_1.png 	deleted:    src_9/AddExpense/AddExpense.js 	deleted:    src_9/AddExpense/EditExpense/EditExpense.js 	deleted:    src_9/AddExpense/EditExpense/index.js 	deleted:    src_9/AddExpense/index.js

--- App.js
+++ App.js
@@ -1,21 +1,69 @@
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
+/* eslint-disable prettier/prettier */
 import React from 'react';
-import Navigation from './Navigation/Navi';
-import MainContainer from './Navigation/MainContainer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-const App = () => {
+
+import HomeScreen from './src/Screens/HomeScreen';
+import AccountScreen from './src/Screens/AccountScreen';
+import AddExpense from './src/Screens/AddExpense';
+import ViewExpense from './src/Screens/ViewExpense';
+
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+// Define screens
+
+const AccountS = () => { /* ... */ };
+const HomeFeedScreen = () => { /* ... */ };
+const ExpenseDetailsScreen = () => { /* ... */ };
+
+// Nested Stack Navigator for Home tab
+const HomeStack = () => (
+  <Stack.Navigator screenOptions={{headerShown: false}}>
+    <Stack.Screen name="HomeScreen" component={HomeScreen} />
+    <Stack.Screen name="AddExpense" component={AddExpense} />
+    <Stack.Screen name="ViewExpense" component={ViewExpense} />
+  </Stack.Navigator>
+);
+
+function App() {
   return (
-    <SafeAreaView style={styles.root}>
-      <Navigation>ok</Navigation>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          tabBarActiveTintColor: 'blue',
+          tabBarInactiveTintColor: 'gray',
+        }}>
+        <Tab.Screen
+          name="Home"
+          component={HomeStack}
+          options={{
+            tabBarLabel: 'Home',
+            headerShown: false,
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="home" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Account"
+          component={AccountScreen}
+          options={{
+            tabBarLabel: 'Account',
+            headerShown: false,
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="account" color={color} size={size} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
-};
-
-const styles = StyleSheet.create({
-  root: {
-    flex: 1,
-    backgroundColor: '#F9FBFC',
-  },
-});
+}
 
 export default App;
+
